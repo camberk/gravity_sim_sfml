62aa5e95118abecaad8103b686f3a3a1a899239a
camberk <cameronberkowitz@icloud.com>
2023-09-24 17:01:53 -0400

    added the Sim class where the simulation will be run from

---
+++ Sim.h
@@ -0,0 +1,21 @@
+#ifndef SIM_H
+#define SIM_H
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+class Sim
+{
+private:
+
+public:
+    Sim();
+    ~Sim();
+};
+
+#endif
